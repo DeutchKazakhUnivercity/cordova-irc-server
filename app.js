6bf1d863d2efa22d3f4ccb64d2c8e1457685544e
Andrey Kurdyumov <kant2002@gmail.com>
2015-05-12 11:20:07 +0600

    Use socket.io for Websocket communications

--- app.js
+++ app.js
@@ -1,47 +1,90 @@
-/*jslint node:true*/
-'use strict';
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
 
-// Read port as the first parameters of the command line.
-var defaultPort = 8080,
-    WebSocketServer = require('ws').Server,
-    app = require('http').createServer(handler),
-    //io = require('socket.io')(app),
-    fs = require('fs'),
-    argv = require('yargs')
-        .usage('Usage: $0 -port [num]')
-        .default('port', process.env.PORT || defaultPort, 'Port on which server would be listening')
-        .argv,
-    wss;
-
-function handler(req, res) {
-    fs.readFile(__dirname + '/index.html',
-        function (err, data) {
-            if (err) {
-                res.writeHead(500);
-                return res.end('Error loading index.html');
-            }
-        
-            res.writeHead(200);
-            res.end(data);
-        });
-}
-
-var port = argv.port;
-wss = new WebSocketServer({ server: app });
-wss.on('connection', function (ws) {
-    console.log('Client connected');
-    ws.on('message', function (message) {
-        console.log('< %s', message);
-        wss.clients.forEach(function each(client) {
-            // Send message back to the 
-            // all connected clients
-            console.log("> %s to client", message);
-            client.send(message);
-        });
-
-    });
-
-    ws.send('Client connected to the server.');
+server.listen(port, function () {
+    console.log('Server listening at port %d', port);
 });
-console.log('IRC server started on port ' + port + '.');
-app.listen(port);
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+// Chatroom
+
+// usernames which are currently connected to the chat
+var usernames = {};
+var numUsers = 0;
+
+/**
+ * Restrict to only WebSockets protocol
+ */
+io.configure(function () {
+    io.set('transports', ['websocket']);
+});
+
+io.configure(function () {
+    io.set('match origin protocol', true);
+});
+
+io.on('connection', function (socket) {
+    var addedUser = false;
+    
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', function (data) {
+        // we tell the client to execute 'new message'
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
+    
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', function (username) {
+        // we store the username in the socket session for this client
+        socket.username = username;
+        // add the client's username to the global list
+        usernames[username] = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+    
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', function () {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+    
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', function () {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+    
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function () {
+        // remove the username from global usernames list
+        if (addedUser) {
+            delete usernames[socket.username];
+            --numUsers;
+            
+            // echo globally that this client has left
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
+});
